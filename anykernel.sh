77b91a834be4d06088a53812088ba8a58a8fcd11
Neebe3289 <neebexd@gmail.com>
2025-12-31 13:43:12 +0700

    anykernel: Applying installation method for specific ROMs
    Certain ROM builds (e.g. Timjosten) require a specific install flow to restore bundled LKMs. Without this, features (e.g. vibration, zram, etc.) that may break or not work after flashing a oss/custom kernel. This change detect a target ROM and applies required installation method, while keeping default install flow for other ROMs.
    
    Test: Boot, check functionality
    Co-authored-by: Wahid Khan <wahidzk0091@gmail.com>
    Change-Id: I9a5253a420013309dd4741372a2aeb02093aa7da
    Signed-off-by: Neebe3289 <neebexd@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -17,17 +17,59 @@ supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
 
+# Kernel installer setup for specific ROM's build.
+# Fix/restore bundled LKM (Loadable Kernel Module) provided by Timjosten.
+# Fixing issue such as vibration, zram, etc. that may break or not work after installing a oss/custom kernel.
+romsign() {
+   local prop=""
+   local cr_version="ro.crdroid.build.version"
+   local evo_version="ro.evolution.build.version"
+
+   for path in /system_root/system/build.prop /system/build.prop; do
+      if [ -f "$path" ]; then
+         prop="$path"
+         break
+      fi
+   done
+
+   [ -z "$prop" ] && return 1
+
+   if grep -qE "$cr_version|$evo_version" "$prop" && grep -q "timjosten" "$prop"; then
+      return 0
+   else
+      return 1
+   fi
+}
+
 # boot shell variables
 BLOCK=/dev/block/platform/bootdevice/by-name/boot;
-IS_SLOT_DEVICE=0;
-RAMDISK_COMPRESSION=auto;
-PATCH_VBMETA_FLAG=auto;
 
-# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
-. tools/ak3-core.sh;
+if romsign; then
+    BLOCK=boot;
+    IS_SLOT_DEVICE=auto;
+    RAMDISK_COMPRESSION=none;
 
-# boot install
-dump_boot;
+    . tools/ak3-core.sh;
 
-write_boot;
-## end boot install
+    ui_print " ";
+    ui_print "Timjosten ROM's detected...";
+    ui_print "Applying fix for vibration, zram, etc.";
+
+    split_boot;
+    patch_cmdline initcall_blacklist initcall_blacklist=
+    flash_boot;
+
+    ui_print " ";
+    ui_print "Installing successfully...";
+else
+    IS_SLOT_DEVICE=0;
+    RAMDISK_COMPRESSION=auto;
+
+    . tools/ak3-core.sh;
+
+    dump_boot;
+    write_boot;
+
+    ui_print " ";
+    ui_print "Installing successfully...";
+fi
